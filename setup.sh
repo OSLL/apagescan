51087f05b97b773344a614213c22c77143f1d2ca
OlgaKochneva <kochnevaolga74@bk.ru>
2020-03-05 19:21:08 +0300

    #2 Added script to setup environment and install requirements

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+python3 -m virtualenv .venv
+source .venv/bin/activate
+python3 -m pip install -r ./requirements.txt
+#pyinstaller --onefile --noconsole main.py
+#cp dist/main apagescan
