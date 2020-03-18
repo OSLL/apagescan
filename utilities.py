131a80c97c0dee54258767fa447eea29f3624db0
OlgaKochneva <kochnevaolga74@bk.ru>
2020-03-18 21:08:07 +0300

    Add return values in utilities.py. Remove empty file after merge

--- utilities.py
+++ utilities.py
@@ -12,6 +12,9 @@ from device_interaction import exec_command
 
 def generate_color():
     """Generates random color
+
+    :return: Color in rgba color model
+    :rtype: QColor
     """
     red = int(random.randrange(0, 255))
     green = int(random.randrange(0, 255))
@@ -24,6 +27,8 @@ def sleep(sec):
     """Suspends (waits) execution of the app for a given number of seconds
 
     :param sec: number of seconds to sleep for
+
+    :return: None
     """
     loop = QEventLoop()
     QTimer.singleShot(sec * 1000, loop.quit)
@@ -33,8 +38,10 @@ def sleep(sec):
 def clean_tmp_data_from_device(device, remove_page_data=True, remove_pids_data=True):
     """Removes collected pagedata from connected android device
 
-    :param remove_page_data: true if page data has to be removed, false if not
-    :param remove_pids_data: true if pids data has to be removed, false if not
+    :param remove_page_data: True if page data has to be removed, False if not
+    :param remove_pids_data: True if pids data has to be removed, False if not
+
+    :return: None
     """
     page_data_mask = '*_page_data'
     pids_file_mask = '*.csv'
@@ -51,9 +58,11 @@ def clean_tmp_data_from_device(device, remove_page_data=True, remove_pids_data=T
 def clean_tmp_data(remove_page_data=True, remove_pictures_data=True, remove_pids_data=True):
     """Removes collected from a device and transferred into app pagedata
 
-    :param remove_page_data: true if page data has to be removed, false if not
-    :param remove_pids_data: true if pids data has to be removed, false if not
+    :param remove_page_data: True if page data has to be removed, False if not
+    :param remove_pids_data: True if pids data has to be removed, False if not
     :param remove_pictures_data: true if pictures data has to be removed, false if not
+
+    :return: None
     """
     page_data_mask = '*_page_data'
     picture_mask = '*.png'
@@ -82,7 +91,9 @@ def create_regions_map(page_data={}):
     """Creates tree of processes memory regions
 
     :param page_data: data about each page for each inspected process
+
     :return: tree of regions represented as (start_pfn, end_pfn, pid)
+    :rtype: IntervalTree
     """
     regions = IntervalTree()
     for pid, data in page_data.items():
